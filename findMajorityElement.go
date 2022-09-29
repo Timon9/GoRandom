9b4104bc609ba4e4ce3a3e5b19c0856196bf1a73
mytaxflow_timon <timon@mytaxflow.dev>
2022-09-29 11:27:17 +0200

    Comment: Instructions

--- findMajorityElement.go
+++ findMajorityElement.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+/*
+
+Given an integer array of size `n`, return the element which appears more than `n/2` times. Assume that the input always contain the majority element.
+
+Input : [2, 8, 7, 2, 2, 5, 2, 3, 1, 2, 2]
+Output: 2
+
+Input : [1, 3, 1, 1]
+Output: 1
+
+*/
+
 func StartFindMajorityElement() {
 	fmt.Println("StartFindMajorityElement")
 }
