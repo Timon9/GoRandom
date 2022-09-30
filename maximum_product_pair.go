24f2fca4a6bb64679661fdfc0b309140cbc12651
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-09-30 16:17:06 +0200

    wip

--- maximum_product_pair.go
+++ maximum_product_pair.go
@@ -23,7 +23,23 @@ Output: null
 
 func sort(input []int) []int {
 
-	return input
+	var r = []int{}
+
+	piv := 1
+	c := 0
+	for i := 0; i < len(input); i++ {
+		v := input[i]
+		if v > piv {
+			//Append after pivot
+			r = append(r, v)
+		} else {
+			//Prepppend before pivot
+			r = append([]int{v}, r...)
+			c++
+		}
+	}
+	fmt.Printf("Current pivot point:%d\n", len(r)-c)
+	return r
 }
 
 func maximumProductPair(input []int) []int {
@@ -34,9 +50,15 @@ func maximumProductPair(input []int) []int {
 	/*
 		Idea: Sort the integers, select the top end 2 and compare with the bottom end 2 (-x*-x=pos)
 	*/
-	input = sort(input)
-
+	fmt.Println("====")
+	fmt.Println("Before sort:")
 	fmt.Println(input)
+
+	input = sort(input)
+	fmt.Println("After sort:")
+	fmt.Println(input)
+
+	fmt.Println("====")
 
 	return maxPair
 
@@ -44,7 +66,7 @@ func maximumProductPair(input []int) []int {
 
 func StartMaximumProductPair() {
 	fmt.Println(maximumProductPair([]int{-10, -3, 5, 6, -2}))
-	fmt.Println(maximumProductPair([]int{-4, 3, 2, 7, -5}))
-	fmt.Println(maximumProductPair([]int{1}))
+	//	fmt.Println(maximumProductPair([]int{-4, 3, 2, 7, -5}))
+	//	fmt.Println(maximumProductPair([]int{1}))
 
 }
