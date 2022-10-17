13da0279e52df76bcb63f5277ca7e1f06f55478c
mytaxflow_timon <timon@mytaxflow.dev>
2022-10-17 12:50:30 +0200

    Find the lowest

--- merge_arrays_II.go
+++ merge_arrays_II.go
@@ -14,6 +14,16 @@ Y[] = [1, 8, 9, 10, 15]
 Output: X[] = [1, 2, 3, 5, 6, 8, 9, 10, 15]
 
 */
+
+func findNextFilledCell(x []int) (int, error) {
+	for i := 0; i < len(x); i++ {
+		if x[i] > 0 {
+			return x[i], nil
+		}
+	}
+	return 0, nil
+}
+
 func solveMergeArraysII(x []int, y []int) {
 	m := len(x)
 	n := len(y)
@@ -23,9 +33,23 @@ func solveMergeArraysII(x []int, y []int) {
 		return
 	}
 
-	for i := 0; i < len(x); i++ {
+	for i := 0; i < m; i++ {
 		v := x[i]
-		fmt.Println(v)
+		r := y[0]
+
+		if v == 0 { // We found a vacant cell
+			if (i + 1) < m { // We have a next cell
+
+				hn, err := findNextFilledCell(x[i:m]) // The highest next cell of X
+
+				if len(y) > 0 && (r < hn || err == nil) { // The lowest of Y is lower than the next filled cell of X , lets use that.
+					fmt.Printf("Swapping from Y #%d for %d\n", i, r)
+				} else if err == nil { // Use the next of X
+					fmt.Printf("Swapping from X #%d for %d\n", i, hn)
+				}
+
+			}
+		}
 	}
 }
 func MergeArraysII() {
