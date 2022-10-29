4a2e5e255c6890c688ec2f1506312e19e1606541
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-10-29 15:31:49 +0200

    ReplaceArrayElements

--- replace_array_elements.go
+++ replace_array_elements.go
@@ -15,17 +15,35 @@ Output: [1152, 1920, 1440, 2880, 960, 720]
 */
 
 func solveReplaceArrayElements(input []int) {
+
 	fmt.Println("ReplaceArrayElements")
-	for i := 0; i < len(input); i++ {
-		v := input[i]
 
-		fmt.Println(v)
-
+	// `left[i]` stores the product of all elements in subarray[0…i-1]
+	left := make(map[int]int)
+	left[0] = 1
+	for i := 1; i < len(input); i++ {
+		left[i] = input[i-1] * left[i-1]
 	}
+
+	// `right[i]` stores the product of all elements in subarray[i+1…n-1]
+	right := make(map[int]int)
+	right[len(input)-1] = 1
+	for ii := len(input) - 2; ii >= 0; ii-- {
+		right[ii] = input[ii+1] * right[ii+1]
+	}
+
+	// replace each element with the product of its left and right subarray
+	for i := 0; i < len(input); i++ {
+		s := left[i] * right[i]
+		fmt.Println(s)
+	}
+	fmt.Println(left)
+	fmt.Println(right)
+
 }
 
 func ReplaceArrayElements() {
 	solveReplaceArrayElements([]int{1, 2, 3, 4, 5})
-	solveReplaceArrayElements([]int{5, 3, 4, 2, 6, 8})
+	// solveReplaceArrayElements([]int{5, 3, 4, 2, 6, 8})
 
 }
