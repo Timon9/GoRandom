8e09fdea087d95a7bfd41a8cd2df742b59d8af18
Timon Scheurwater <timonscheurwater@protonmail.com>
2022-11-04 14:02:27 +0100

    Done! solveLongestBitonicSubarray

--- longest_bitonic_subarray.go
+++ longest_bitonic_subarray.go
@@ -16,34 +16,29 @@ Output: [-5, -1, 0, 2, 1] or [1, 6, 5, 4, 2]
 
 */
 
-func LongestBitonicSubarray() {
+func solveLongestBitonicSubarray(input []int) {
 	fmt.Println("LongestBitonicSubarray")
-	input := []int{3, 5, 8, 4, 5, 9, 10, 8, 5, 3, 4}
-	fmt.Println(input)
 
 	// Build ascending auxiliary array
 	a := make([]int, len(input))
-	a[0] = 1
-	c := 1
-	for i := 1; i < len(input); i++ {
-		if input[i-1] < input[i] {
-			c++
-		} else {
-			c = 1
-		}
-		a[i] = c
-	}
-	fmt.Println("ASC", a)
-
-	// Build descending auxiliary array
-	d := make([]int, len(input))
-	d[len(input)-1] = 1
-	c = 1
+	c := 0
 	for i := len(input) - 2; i >= 0; i-- {
 		if input[i+1] > input[i] {
 			c++
 		} else {
-			c = 1
+			c = 0
+		}
+		a[i] = c
+	}
+
+	d := make([]int, len(input))
+	d[0] = 0
+	c = 0
+	for i := len(input) - 2; i >= 0; i-- {
+		if input[i+1] < input[i] {
+			c++
+		} else {
+			c = 0
 		}
 		d[i] = c
 	}
@@ -51,18 +46,23 @@ func LongestBitonicSubarray() {
 	max_length := 1
 	m := make(map[int]int)
 
-	for i := 0; i < len(input); i++ {
-		v := a[i] + d[i]
+	for i := 0; i < len(input)-1; i++ {
+		v := a[i] + d[i+a[i]]
 		if v > max_length {
 			max_length = v
-			m[0] = v            // value
-			m[1] = input[i]     // current value input
-			m[2] = i - d[i] + 1 // start highest bitonic array
-			m[3] = i + a[i] - 1 // end highest bitonic array
+			m[0] = v         // value
+			m[1] = input[i]  // current value input
+			m[2] = i         // start highest bitonic array
+			m[3] = i + v + 1 // end highest bitonic array
 		}
 	}
-	//	fmt.Println(m)
 
-	fmt.Println("DESC", d)
 	fmt.Println("Result", input[m[2]:m[3]])
+
+}
+
+func LongestBitonicSubarray() {
+
+	solveLongestBitonicSubarray([]int{3, 5, 8, 4, 5, 9, 10, 8, 5, 3, 4})
+	solveLongestBitonicSubarray([]int{-5, -1, 0, 2, 1, 6, 5, 4, 2})
 }
